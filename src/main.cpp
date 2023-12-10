5d50e6a4c4856105af5c3278da4bb8d1ef912f94
suppalerk prarakkamo <suppalerk1561@gmail.com>
2023-12-10 16:21:01 +0700

    finist_no_switchstate

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,8 @@
 #define MQTT_EVT_TOPIC  "6424020002/smartfarm/control"
 // #define MQTT_CMD_TOPIC  "smartfarm/status"
 #define MQTT_CMD_TOPIC  "6424020002/smartfarm/status"
+// #define MQTT_CMD_TOPIC  "6424020002/smartfarm/control"
+
 
 #define MQTT_DEV_ID     43
 
@@ -29,7 +31,42 @@ static bool enable_flag = true;
 StaticJsonDocument<128> cmd_buf;
 StaticJsonDocument<128> evt_buf;
 
+// State Declaration
+float temp=0, humi=0, mois=0, ligh=0;
+#define INI 0
+int state=INI;
+#define ONCH1 5
+#define ONCH2 6
+#define ONCH3 7
+#define ONCH4 8
+#define OFFCH1 9
+#define OFFCH2 10
+#define OFFCH3 11
+#define OFFCH4 12
+#define WAITING 13
+
+DynamicJsonDocument doc(1024);
+
+
 static char buf[128];
+
+//relay setup ____________________________________ start
+#define RELAY_CH_1  4
+#define RELAY_CH_2  27
+#define RELAY_CH_3  16
+#define RELAY_CH_4  17
+
+
+void relay_setup(){
+    pinMode(RELAY_CH_1, OUTPUT);
+    pinMode(RELAY_CH_2, OUTPUT);
+    pinMode(RELAY_CH_3, OUTPUT);
+    pinMode(RELAY_CH_4, OUTPUT);
+    digitalWrite(RELAY_CH_1, HIGH);
+    digitalWrite(RELAY_CH_2, HIGH);
+    digitalWrite(RELAY_CH_3, HIGH);
+    digitalWrite(RELAY_CH_4, HIGH);
+    }
 
 //dht setup ____________________________________ start
 #define DHTPIN 26    // Digital pin connected to the DHT sensor 
@@ -53,13 +90,17 @@ uint32_t delayMS;
 void setup() {
   Serial.begin(115200);
   print_memory();
+
+  //setup relay ____________________________
+  relay_setup();
+
   // pinMode(BTN_PIN, INPUT_PULLUP);
   pinMode(BTN_PIN, INPUT_PULLUP);
   // // connect to WiFi
   // net_mqtt_init(WIFI_SSID, WIFI_PASSWORD);
   net_mqtt_init(WIFI_SSID, WIFI_PASSWORD);  
   // // connect to MQTT broker
-  // net_mqtt_connect(MQTT_DEV_ID,MQTT_CMD_TOPIC, mqtt_callback);
+
   net_mqtt_connect(MQTT_DEV_ID,MQTT_CMD_TOPIC, mqtt_callback);
   Serial.println("Setup mqtt done...");
 
@@ -93,91 +134,148 @@ void setup() {
   //setup dht ____________________________
 }
 
+// void dht11_loop(){
+//   delay(delayMS);
+//   // Get temperature event and print its value.
+//   sensors_event_t event;
+//   dht.temperature().getEvent(&event);
+//   if (isnan(event.temperature)) {
+//     Serial.println(F("Error reading temperature!"));
+//   }
+//   else {
+//     Serial.print(F("Temperature: "));
+//     Serial.print(event.temperature);
+//     Serial.println(F("°C"));
+//   }
+//   // Get humidity event and print its value.
+//   dht.humidity().getEvent(&event);
+//   if (isnan(event.relative_humidity)) {
+//     Serial.println(F("Error reading humidity!"));
+//   }
+//   else {
+//     Serial.print(F("Humidity: "));
+//     Serial.print(event.relative_humidity);
+//     Serial.println(F("%"));
+//   }
+// }
+
 // Main loop
 void loop() {
   static uint32_t prev_millis = 0;
 
-  delay(delayMS);
-  // Get temperature event and print its value.
-  sensors_event_t event;
-  dht.temperature().getEvent(&event);
-  if (isnan(event.temperature)) {
+//   switch (state)
+//   {
+//     case INI:{
+//         Serial.println("Initializing");
+//         digitalWrite(RELAY_CH_1,LOW);
+//         digitalWrite(RELAY_CH_2,LOW);
+//         digitalWrite(RELAY_CH_3,LOW);
+//         digitalWrite(RELAY_CH_4,LOW);
+
+//         state=WAITING;
+//         break;  
+//     }
+//     case WAITING:{
+//         Serial.println("Waiting...");
+//         break;  
+//     }
+//     case ONCH1:{
+//         Serial.println("CH1 On");
+//         digitalWrite(RELAY_CH_1,HIGH);
+//         state=WAITING;
+//         break;  
+//     }
+//     case ONCH2:{
+//         Serial.println("CH2 On");
+//         digitalWrite(RELAY_CH_2,HIGH);
+//         state=WAITING;
+//         break;  
+//     }
+//     case ONCH3:{
+//         Serial.println("CH3 On");
+//         digitalWrite(RELAY_CH_3,HIGH);
+//         state=WAITING;
+//         break;  
+//     }
+//     case ONCH4:{
+//         Serial.println("CH4 On");
+//         digitalWrite(RELAY_CH_4,HIGH);
+//         state=WAITING;
+//         break;  
+//     }
+//     case OFFCH1:{
+//         Serial.println("CH1 Off");
+//         digitalWrite(RELAY_CH_1,LOW);
+//         state=WAITING;
+//         break;  
+//     }
+//     case OFFCH2:{
+//         Serial.println("CH2 Off");
+//         digitalWrite(RELAY_CH_2,LOW);
+//         state=WAITING;
+//         break;  
+//     }
+//     case OFFCH3:{
+//         Serial.println("CH3 Off");
+//         digitalWrite(RELAY_CH_3,LOW);
+//         state=WAITING;
+//         break;  
+//     }
+//     case OFFCH4:{
+//         Serial.println("CH4 Off");
+//         digitalWrite(RELAY_CH_4,LOW);
+//         state=WAITING;
+//         break;  
+//     }
+//   }
+
+//   dht11_loop();
+
+// Get temperature event and print its value.
+    delay(delayMS);
+    // Get temperature event and print its value.
+    sensors_event_t event;
+    dht.temperature().getEvent(&event);
+    if (isnan(event.temperature)) {
     Serial.println(F("Error reading temperature!"));
-  }
-  else {
+    }
+    else {
     Serial.print(F("Temperature: "));
+    temp=event.temperature;
     Serial.print(event.temperature);
     Serial.println(F("°C"));
-  }
-  // Get humidity event and print its value.
-  dht.humidity().getEvent(&event);
-  if (isnan(event.relative_humidity)) {
+    }
+    // Get humidity event and print its value.
+    dht.humidity().getEvent(&event);
+    if (isnan(event.relative_humidity)) {
     Serial.println(F("Error reading humidity!"));
-  }
-  else {
+    }
+    else {
     Serial.print(F("Humidity: "));
+    humi=event.relative_humidity;
     Serial.print(event.relative_humidity);
     Serial.println(F("%"));
-  }
+    }
 
-    evt_buf["id"] = "001";
-    evt_buf["humi"] = event.relative_humidity;
-    evt_buf["temp"] = event.temperature;
-    evt_buf["mois"] = "50";
-    evt_buf["ligh"] = "50";
-    evt_buf["ch1"] = "off";
-    evt_buf["ch2"] = "off";
-    evt_buf["ch3"] = "off";
-    evt_buf["ch4"] = "";
-    serializeJson(evt_buf,buf);
-    net_mqtt_publish(MQTT_EVT_TOPIC,buf);
-  // if (enable_flag) {
-    // check button status
-    // if(digitalRead(BTN_PIN)==LOW){
-    //   ESP_LOGI(TAG, "Total");
-    // // check button bounce
-    //     if(millis()-prev_millis > 1000){
-    //     // publish button event
-    //         prev_millis = millis();
-    //         // evt_buf["timestamp"] = millis();
-    //         evt_buf["id"] = "001";
-    //         evt_buf["humi"] = "100";
-    //         evt_buf["temp"] = "100";
-    //         evt_buf["mois"] = "100";
-    //         evt_buf["ligh"] = "100";
-    //         evt_buf["ch1"] = "on";
-    //         evt_buf["ch2"] = "on";
-    //         evt_buf["ch3"] = "on";
-    //         evt_buf["ch4"] = "on";
-    //         serializeJson(evt_buf,buf);
-    //         net_mqtt_publish(MQTT_EVT_TOPIC,buf);
 
-    //         // {"id":"001","humi":1,"temp":1,"mois":1,"ligh":1,"ch1":"on","ch2":"on","ch3":"on","ch4":"on"}
-    //     }
-    // }
-  
+  evt_buf["id"] = "001";
+  evt_buf["humi"] = humi;
+  ESP_LOGI(TAG, "Total heap: %u", humi);
+  evt_buf["temp"] = temp;
+  ESP_LOGI(TAG, "Total heap: %u", temp);
+//   evt_buf["temp"] = event.temperature;
+  evt_buf["mois"] = "50";
+  evt_buf["ligh"] = "50";
+  // evt_buf["ch1"] = "off";
+  // evt_buf["ch2"] = "off";
+  // evt_buf["ch3"] = "off";
+  // evt_buf["ch4"] = "";
+  serializeJson(evt_buf,buf);
+  net_mqtt_publish(MQTT_EVT_TOPIC,buf);
+    
   net_mqtt_loop();
   delay(100);
   }
-
- // if (enable_flag) {
-  //   // check button status
-  //   if(digitalRead(BTN_PIN)==LOW){
-  //   // check button bounce
-  //     if(millis()-prev_millis > 1000){
-  //       // publish button event
-  //       prev_millis = millis();
-  //       evt_buf["ID"] = MQTT_DEV_ID;
-  //       evt_buf["timestamp"] = millis();
-  //       evt_buf["pressed"] = true;
-  //       serializeJson(evt_buf,buf);
-  //       net_mqtt_publish(MQTT_EVT_TOPIC,buf);
-  //       }
-  //   }
-  // }
-
-  // loop MQTT interval
-
 
 // Print memory information
 void print_memory() {
@@ -188,37 +286,39 @@ void print_memory() {
 }
 
 
-
 // callback function to handle MQTT message
 void mqtt_callback(char* topic, byte* payload, unsigned int length) {
-  String msg;
-  String key = "cmd";
+// String msg;
+//   String key = "cmd";
   ESP_LOGI(TAG, "Message arrived on topic %s", topic);
   ESP_LOGI(TAG, "Payload: %.*s", length, payload);
   // ESP_LOGI(TAG, "Payload: %s", payload);
   // msg = String((char *)payload).substring(2,length);
-  msg = String((char *)payload).substring(7,length-2);
+//   msg = String((char *)payload).substring(7,length-2);
   // msg = String((char *)payload).substring(0,length);
-  // msg = msg.substring(msg.indexOf(key)+5,msg.indexOf(key)+11);
+  // msg = msg.substring(msg.indexOf(key)+5,msg.indexOf(key)+11);  
 
+//   ESP_LOGI(TAG, "Payload: %s", msg.c_str());
 
+    //doc test
+    deserializeJson(doc, payload);
 
-  // ESP_LOGI(TAG, "Payload: %s", msg.c_str());
-  // ESP_LOGI(TAG, "Payload: %s", msg);
-  // if (msg == "on")
-  // {
-  //   ESP_LOGI(TAG, "EAAAAAA");
-  // }
-  // else if (msg != "on")
-  // {
-  //   ESP_LOGI(TAG, "");
-  // }
-  
+    int ch1Value = doc["ch1"];
+    Serial.print("doc: ");
+    Serial.println(ch1Value);
+    
+    if (doc["ch1"]==1)   { state=ONCH1;  Serial.println(" CH1 On"); digitalWrite(RELAY_CH_1,LOW);}
+    if (doc["ch1"]==0)  { state=OFFCH1; Serial.println(" CH1 Off"); digitalWrite(RELAY_CH_1,HIGH);}
+    if (doc["ch2"]==1)   { state=ONCH2;  Serial.println(" CH2 On"); digitalWrite(RELAY_CH_2,LOW);}
+    if (doc["ch2"]==0)  { state=OFFCH2; Serial.println(" CH2 Off"); digitalWrite(RELAY_CH_2,HIGH);}
+    if (doc["ch3"]==1)   { state=ONCH3;  Serial.println(" CH3 On"); digitalWrite(RELAY_CH_3,LOW);}
+    if (doc["ch3"]==0)  { state=OFFCH3; Serial.println(" CH3 Off"); digitalWrite(RELAY_CH_3,HIGH);}
+    if (doc["ch4"]==1)   { state=ONCH4;  Serial.println(" CH4 On"); digitalWrite(RELAY_CH_4,LOW);}
+    if (doc["ch4"]==0)  { state=OFFCH4; Serial.println(" CH4 Off"); digitalWrite(RELAY_CH_4,HIGH);}
+    Serial.println("-----------------------------");
+    // Serial.println("CH1: "+String(doc["ch1"]));
 
   // ESP_LOGI(TAG, "Payload: %s", length);
-
-
-
   // ESP_LOGI(TAG, "Payload: %.*s", length, );
   // Serial.println("Message arrived on topic"+ String(topic));
   // Serial.println("Payload: %.*s", length, payload);
